2c065ebfa3d99d79bd41a1184c5a22a29a9f87de
Lulinw <1226523508@qq.com>
2024-10-24 22:32:22 +0800

    线程池的初步使用。（有点烂写的）

---
+++ backend/src/test/java/com/aw/aidati/QuestionControllerTest.java
@@ -0,0 +1,36 @@
+package com.aw.aidati;
+
+import com.aw.aidati.controller.QuestionController;
+import com.aw.aidati.model.dto.question.AiGenerateQuestionRequest;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+public class QuestionControllerTest {
+
+    @Resource
+    private QuestionController questionController;
+
+    @Test
+    void aiGenerateQuestionSSETest() throws InterruptedException {
+        // 模拟调用
+        AiGenerateQuestionRequest aiGenerateQuestionRequest = new AiGenerateQuestionRequest();
+        aiGenerateQuestionRequest.setAppId(3L);
+        aiGenerateQuestionRequest.setQuestionNumber(10);
+        aiGenerateQuestionRequest.setOptionNumber(2);
+
+        // 模拟普通用户
+        questionController.aiGenerateQuestionSSETest(aiGenerateQuestionRequest, false);
+        // 模拟普通用户
+        questionController.aiGenerateQuestionSSETest(aiGenerateQuestionRequest, false);
+        // 模拟会员用户
+        questionController.aiGenerateQuestionSSETest(aiGenerateQuestionRequest, true);
+
+        // 模拟主线程一直启动
+        Thread.sleep(1000000L);
+    }
+}
+
